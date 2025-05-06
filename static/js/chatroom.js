90c44c21ee499543fb39b92d0f003edcfad4c627
KillAllChickens <admin@killallchickens.org>
2025-05-06 06:39:32 -0500

    ADD: 5mb image limit

--- static/js/chatroom.js
+++ static/js/chatroom.js
@@ -460,6 +460,11 @@ function addSystemMessage(message, nickname, timestamp) {
 // }
 
 function openImageOptions(file) {
+  // Add size limit (4mb)
+  if (file.size > 5 * 1024 * 1024) {
+    alert("No image larger than 5mb allowed!\nif you would like to compress your image you can use imagecompressor.com or resizepixel.com");
+    return;
+  }
   // console.log('Opening image options:);
   imageOption.style.display = 'block';
   // imageOptionInner.style.display = 'flex';
